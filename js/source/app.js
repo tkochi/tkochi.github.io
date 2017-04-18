3e24cb04ec2fc8d77014359d90f6d97e864e908f
Takayoshi Kochi (MacBook Pro) <takayoshi.kochi@gmail.com>
2017-04-18 20:36:52 +0900

    Add an example

---
+++ js/source/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+ReactDOM.render(
+  <h1>
+    <Logo /> Welcome!
+  </h1>,
+  document.getElementById('app')
+);
